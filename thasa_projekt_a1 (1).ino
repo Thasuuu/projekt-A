d5e786316fdc4609a2248ea2ddbb3d642448ba51
Thasuuu <32567759+Thasuuu@users.noreply.github.com>
2019-05-11 02:19:19 +0200

    Update thasa_projekt_a1 (1).ino

--- thasa_projekt_a1 (1).ino
+++ thasa_projekt_a1 (1).ino
@@ -1,16 +1,3 @@
-int sensor = 0;
-
-int btn_one = 0;
-
-int btn_to = 0;
-
-int LOW2 = 0;
-
-int push_one = 0;
-
-int i = 0;
-
-int push_to = 0;
 
 void setup()
 {
@@ -22,15 +9,24 @@ void setup()
   pinMode(11, INPUT);
 }
 
+
+const int LOCKED = 1;
+const int WAIT_FOR_ONE = 2;
+const int WAIT_FOR_TO = 3;
+const int CHEKING = 4;
+const int UNLOCKED = 5;
+int state = 0;
+
+int push_one = 0;
+int push_to = 0;
+
 void loop()
 {
-  digitalWrite(13, HIGH);
-  digitalWrite(12, LOW);
-  digitalWrite(9, HIGH);
+ 
 
-  sensor = digitalRead(2);
-  btn_one = digitalRead(10);
-  btn_to = digitalRead(11);
+ int sensor = digitalRead(2);
+ int btn_one = digitalRead(10);
+ int btn_to = digitalRead(11);
 
   if (btn_one == LOW) {
     push_one = 0;
@@ -39,5 +35,42 @@ void loop()
   if (btn_to == LOW) {
     push_to = 0;
   }
-  delay(10); // Delay a little bit to improve simulation performance
+  
+  switch (state) {
+      
+    case LOCKED:
+        digitalWrite(13, HIGH);
+      
+      if(sensor == HIGH) {
+        digitalWrite(7, LOW);
+        state = WAIT_FOR_ONE;
+      }
+      break;
+      
+    case WAIT_FOR_ONE:
+      digitalWrite(12,HIGH);
+      if ((btn_one == HIGH) && (!push_one)) {
+        push_one = 1;
+        digitalWrite(12, LOW)
+     delay(200); // Delay a little bit to improve simulation performance
+        state = WAIT_FOR_TO;
 }
+      
+      
+        if ((btn_to == HIGH) && (!push_to)) {
+        push_to = 1;
+        digitalWrite(12, LOW)
+     delay(200); // Delay a little bit to improve simulation performance
+        state = WAIT_FOR_TO;
+          
+        }
+      
+      break;
+      
+      
+  }
+}
+
+     
+          
+          
